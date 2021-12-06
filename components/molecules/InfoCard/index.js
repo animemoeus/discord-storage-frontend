7b59d2d02cebcece759ba7ea3967fdb2f9fbe397
Arter Tendean <arter.tendean.07@gmail.com>
2021-12-06 11:22:29 +0800

    update InfoCard

--- components/molecules/InfoCard/index.js
+++ components/molecules/InfoCard/index.js
@@ -1,27 +1,36 @@
 import { useEffect, useState } from "react";
 
+import moment from "moment";
 import Card from "react-bootstrap/Card";
 import CountUp from "react-countup";
-import moment from "moment";
+import Placeholder from "react-bootstrap/Placeholder";
 
 export default function InfoCard() {
   const [info, setInfo] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
 
-  // get data from API when page load
   useEffect(() => {
-    getData();
+    // wait 2.5s before getting data
+    // why? coz i like the loading placeholder animation :v
+    setTimeout(() => getData(), 2500);
   }, []);
 
   // get data from API
   const getData = () => {
-    var requestOptions = {
+    const requestOptions = {
       method: "GET",
       redirect: "follow",
     };
 
     fetch("https://discord-storage.animemoe.us/info/", requestOptions)
       .then((response) => response.json())
-      .then((result) => setInfo(result))
+      .then((result) => {
+        // save data from API to state
+        setInfo(result);
+
+        // disable placeholder animation state
+        setIsLoading(false);
+      })
       .catch((error) => alert("Can't connect to the Discord Storage server."));
   };
 
@@ -32,26 +41,47 @@ export default function InfoCard() {
           <h2 className="fs-4">(¬‿¬)</h2>
         </Card.Header>
         <Card.Body className="p-2">
-          <p>
-            <b>
-              <span className="fs-3">
-                <CountUp
-                  start={0}
-                  end={info.uploaded_file}
-                  duration={3.3}
-                  separator="."
-                />
-              </span>
-            </b>
-            <br />
-            <span className="fs-5">Files have been uploaded so far.</span>
-          </p>
-          <p className="text-muted">
-            <small>
-              Last updated:{" "}
-              {moment(info.updated_at).local().format("YYYY-MM-DD HH:mm")}
-            </small>
-          </p>
+          {isLoading === true && (
+            <>
+              <div className="animate__animated animate__fadeIn">
+                <Placeholder as="p" animation="wave">
+                  <Placeholder xs={5} size="lg" className="rounded" />
+                </Placeholder>
+
+                <Placeholder as="p" animation="wave">
+                  <Placeholder xs={11} className="rounded" />
+                </Placeholder>
+
+                <Placeholder as="p" animation="wave">
+                  <Placeholder xs={7} size="sm" className="rounded" />
+                </Placeholder>
+              </div>
+            </>
+          )}
+          {isLoading === false && (
+            <div className="animate__animated animate__fadeIn p-0 m-0">
+              <p>
+                <b>
+                  <span className="fs-3">
+                    <CountUp
+                      start={0}
+                      end={info.uploaded_file}
+                      duration={5}
+                      separator="."
+                    />
+                  </span>
+                </b>
+                <br />
+                <span className="fs-5">Files have been uploaded so far.</span>
+              </p>
+              <p className="text-muted">
+                <small>
+                  Last updated:{" "}
+                  {moment(info.updated_at).local().format("YYYY-MM-DD HH:mm")}
+                </small>
+              </p>
+            </div>
+          )}
         </Card.Body>
       </Card>
     </div>
--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import "../styles/globals.css";
+import "animate.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function MyApp({ Component, pageProps }) {
